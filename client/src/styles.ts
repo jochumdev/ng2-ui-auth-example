41629decd61deeb8e3d5810f7ba09e602df4a882
Rene Jochum <rene@jochums.at>
2017-07-17 12:57:35 +0200

    Fix toastr z-index
    Signed-off-by: Rene Jochum <rene@jochums.at>

--- client/src/styles.ts
+++ client/src/styles.ts
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.css';
 import 'ng2-toastr/ng2-toastr.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import 'animate.css/animate.min.css';
 import './css/styles.css';
